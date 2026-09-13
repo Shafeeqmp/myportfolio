b46b4deff890ae8a20fd07e0ccf3e49a721652d1
Muhammed Shafeeq <2009shafeq@gmail.com>
2026-09-13 21:22:38 +0530

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Background from "./components/background";
+import HeroSection from "./components/herosection";
+
 export default function Home() {
   return (
     <>
       <Background />
+      <HeroSection />
     </>
   );
 }
