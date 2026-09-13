f823277451506837646e7c8007edc5c6e6a13cc7
Muhammed Shafeeq <2009shafeq@gmail.com>
2026-09-13 21:03:42 +0530

    error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Background from "./components/background";
-import HeroSection from "./components/herosection"
 export default function Home() {
   return (
     <>
       <Background />
-      <HeroSection />
     </>
   );
 }
